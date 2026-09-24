009f8de1562456be676a10c9d7d864699723a8a4
Kai Pereira <kaipereira2020@gmail.com>
2026-09-24 15:25:30 -0400

    adding a.i

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 console.log("What's done in the dark will always fine a way to shine.")
 
 
-const keywords = ["ai", "llm", "agent", "gpt", "claude", "anthropic", "gemini", "model", "openai", "vibecode", "chatgpt", "copilot", "grok"]
+const keywords = ["ai", "llm", "agent", "gpt", "claude", "anthropic", "gemini", "model", "openai", "vibecode", "chatgpt", "copilot", "grok", "a.i"]
 
 const current_url = window.location.href;
 
